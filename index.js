4803dd0e6387a80288aba692739483a3f2a9894b
Tom Gallacher <tomgallacher23@gmail.com>
2015-03-05 17:45:30 +0000

    ARGUMENTS

--- index.js
+++ index.js
@@ -7,32 +7,39 @@ var LOOKUP = {
   'undefined': 'text-warning'
 };
 
+module.exports = function (opts) {
+  return {
+    jadify: through(function (data) {
+      var queue = [];
+      var overall = 'passed';
+      if (opts.header) {
+      queue.push('header ' + opts.header + EOL);
+      }
+      queue.push('div.panel.panel-default' + EOL);
+      queue.push('  div.panel-heading' + EOL);
+      queue.push('    h3.{color} ' + data.keyword + ': ' + data.name + EOL); // Colour text
+      queue.push('  div.panel-body ' + data.description + EOL);
+      data.elements.forEach(function (el) {
+      queue.push('    div.row' + EOL);
+      queue.push('      div.col-sm-offset-2.col-sm-10' + EOL);
+      queue.push('        h3 ' + el.keyword + ': ' + el.name + EOL);
+      el.steps.forEach(function (step) {
+      queue.push('      div.col-sm-offset-4.col-sm-10' + EOL);
+      queue.push('        h4.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
+        if (step.result.status === 'undefined' && overall !== 'failed') {
+          overall = 'undefined';
+        } else if (step.result.status === 'failed') {
+          overall = 'failed';
+        }
+      });
+      });
+      queue.push(EOL);
 
-module.exports.jadify = through(function (data) {
-  var queue = [];
-  var overall = 'passed';
+      queue.forEach(function (item) {
+        this.queue(item.replace('{color}', LOOKUP[overall]));
+      }.bind(this));
+    })
+  };
+}
 
-  queue.push('div.panel.panel-default' + EOL);
-  queue.push('  div.panel-heading' + EOL);
-  queue.push('    h3.{color} ' + data.keyword + ': ' + data.name + EOL); // Colour text
-  queue.push('  div.panel-body ' + data.description + EOL);
-  data.elements.forEach(function (el) {
-  queue.push('    div.row' + EOL);
-  queue.push('      div.col-sm-offset-2.col-sm-10' + EOL);
-  queue.push('        h3 ' + el.keyword + ': ' + el.name + EOL);
-  el.steps.forEach(function (step) {
-  queue.push('      div.col-sm-offset-4.col-sm-10' + EOL);
-  queue.push('        h4.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
-    if (step.result.status === 'undefined' && overall !== 'failed') {
-      overall = 'undefined';
-    } else if (step.result.status === 'failed') {
-      overall = 'failed';
-    }
-  });
-  });
-  queue.push(EOL);
-
-  queue.forEach(function (item) {
-    this.queue(item.replace('{color}', LOOKUP[overall]));
-  }.bind(this));
-});
+;
