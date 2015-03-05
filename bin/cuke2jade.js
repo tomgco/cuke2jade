ed839eb97cc22c0f969a4596f421873d8b3d8eb1
Tom Gallacher <tomgallacher23@gmail.com>
2015-03-05 17:25:00 +0000

    Adding cli flags
    --bootstrap
    --no-bootstrap
    --css file1.css,file2.css

--- bin/cuke2jade.js
+++ bin/cuke2jade.js
@@ -3,7 +3,18 @@
 // Only work on std-in at the moment
 var cukeme = require('../');
 var JSONStream = require('JSONStream');
-process.stdout.write('link(rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css")\n');
+var argv = require('argh').argv;
+
+if (argv.bootstrap || argv.bootstrap === undefined) {
+  process.stdout.write('link(rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css")\n');
+}
+
+if (argv.css) {
+  argv.css.split(',').forEach(function (item) {
+    process.stdout.write('link(rel="stylesheet" href="' + item + '")\n');
+  });
+}
+
 process.stdin
   .pipe(JSONStream.parse('*'))
   .pipe(cukeme.jadify)
