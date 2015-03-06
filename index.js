66401418902ac71c324f47e918ff920716808104
Tom Gallacher <tomgallacher23@gmail.com>
2015-03-06 15:59:25 +0000

    Fixing merge conflict added colour back to Feature

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ module.exports = function (opts) {
       queue.push('      article.col-md-offset-1.col-md-10' + EOL);
       queue.push('        div.panel.panel-default' + EOL);
       queue.push('          div.panel-heading' + EOL);
-      queue.push('            h4 ' + data.keyword + ': ' + data.name + EOL);
+      queue.push('            h4.{color} ' + data.keyword + ': ' + data.name + EOL);
       queue.push('          div.panel-body ' + data.description + EOL);
       data.elements.forEach(function (el) {
-        queue.push('            article.row' + EOL);
-        queue.push('              h4.col-md-12 ' + el.keyword + ': ' + el.name + EOL);
-        el.steps.forEach(function (step) {
-          queue.push('              div.col-sm-offset-3.col-sm-9' + EOL);
-          queue.push('                h5.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
-            if (step.result.status === 'undefined' && overall !== 'failed') {
-              overall = 'undefined';
-            } else if (step.result.status === 'failed') {
-              overall = 'failed';
-            }
-          });
-        queue.push('            hr' + EOL);
+      queue.push('            article.row' + EOL);
+      queue.push('              h4.col-md-12 ' + el.keyword + ': ' + el.name + EOL);
+      el.steps.forEach(function (step) {
+        queue.push('              div.col-sm-offset-3.col-sm-9' + EOL);
+        queue.push('                h5.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
+          if (step.result.status === 'undefined' && overall !== 'failed') {
+            overall = 'undefined';
+          } else if (step.result.status === 'failed') {
+            overall = 'failed';
+          }
+        });
+      queue.push('            hr' + EOL);
       });
       queue.push(EOL);
 
