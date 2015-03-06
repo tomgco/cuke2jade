7be2bd5d52c995c236b521db6bf22a622e70a2f1
Ian Crowther <ian@iancrowther.co.uk>
2015-03-06 15:19:40 +0000

    updated report structure

--- index.js
+++ index.js
@@ -7,33 +7,42 @@ var LOOKUP = {
   'undefined': 'text-warning'
 };
 
-module.exports.jadify = through(function (data) {
-  var queue = [];
-  var overall = 'passed';
+module.exports = function (opts) {
+  return {
+    jadify: through(function (data) {
+      var queue = [];
+      var overall = 'passed';
+      if (opts.header) {
+        queue.push('header.row' + EOL);
+        queue.push('  h1.col-md-offset-1.col-md-11 ' + opts.header + EOL);
+      }
+      queue.push('section' + EOL);
+      queue.push('  div.row' + EOL);
+      queue.push('    div.col-md-offset-1.col-md-10' + EOL);
+      queue.push('      article.col-md-offset-1.col-md-10' + EOL);
+      queue.push('        div.panel.panel-default' + EOL);
+      queue.push('          div.panel-heading' + EOL);
+      queue.push('            h4 ' + data.keyword + ': ' + data.name + EOL);
+      queue.push('          div.panel-body ' + data.description + EOL);
+      data.elements.forEach(function (el) {
+      queue.push('            article.row' + EOL);
+      queue.push('              h4.col-md-12 ' + el.keyword + ': ' + el.name + EOL);
+      el.steps.forEach(function (step) {
+        queue.push('              div.col-sm-offset-3.col-sm-9' + EOL);
+        queue.push('                h5.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
+          if (step.result.status === 'undefined' && overall !== 'failed') {
+            overall = 'undefined';
+          } else if (step.result.status === 'failed') {
+            overall = 'failed';
+          }
+        });
+      queue.push('            hr' + EOL);
+      });
+      queue.push(EOL);
 
-  this.queue('div.container' + EOL);
-  queue.push('  header' + EOL);
-  queue.push('  div.panel.panel-default' + EOL);
-  queue.push('    div.panel-heading' + EOL);
-  queue.push('      h3.{color} ' + data.keyword + ': ' + data.name + EOL); // Colour text
-  queue.push('    div.panel-body ' + data.description + EOL);
-  data.elements.forEach(function (el) {
-  queue.push('      div.row' + EOL);
-  queue.push('        div.col-sm-offset-2.col-sm-10' + EOL);
-  queue.push('          h3 ' + el.keyword + ': ' + el.name + EOL);
-  el.steps.forEach(function (step) {
-  queue.push('        div.col-sm-offset-4.col-sm-10' + EOL);
-  queue.push('          h4.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
-    if (step.result.status === 'undefined' && overall !== 'failed') {
-      overall = 'undefined';
-    } else if (step.result.status === 'failed') {
-      overall = 'failed';
-    }
-  });
-  });
-  queue.push(EOL);
-
-  queue.forEach(function (item) {
-    this.queue(item.replace('{color}', LOOKUP[overall]));
-  }.bind(this));
+      queue.forEach(function (item) {
+        this.queue(item.replace('{color}', LOOKUP[overall]));
+      }.bind(this));
+    })
+  }
 });
