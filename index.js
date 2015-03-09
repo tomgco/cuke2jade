c479a3a608a8e5d22a07555388e09751f7753a7e
Ian Crowther <ian@iancrowther.co.uk>
2015-03-09 18:03:47 +0000

    Adding dashboard theme to cuke

--- index.js
+++ index.js
@@ -21,41 +21,40 @@ module.exports = function (opts) {
   };
   return {
     jadify: through(function (data) {
-      var group = '';
-      queue.push('section' + EOL);
-      queue.push('  div.row' + EOL);
-      queue.push('    div.col-md-offset-1.col-md-10' + EOL);
-      queue.push('      article.col-md-offset-1.col-md-10' + EOL);
-      queue.push('        div.panel.panel-default' + EOL);
-      queue.push('          div.panel-heading' + EOL);
-      queue.push('            a(href="#'+data.id+'")' + EOL);
-      queue.push('              h4.{color}#' + data.id + ' ' + data.keyword + ': ' + data.name + EOL);
-      if (data.description) {
-      queue.push('            p.h6 ' + data.description + EOL);
-      }
-      queue.push('          div.panel-body ' + EOL);
+      var group = 'passed';
+      queue.push('  div.col-md-6' + EOL);
+      queue.push('    div.panel.panel-default' + EOL);
+      queue.push('      div.panel-heading' + EOL);
+      queue.push('        a(href="#'+data.id+'")' + EOL);
+      queue.push('          h4#' + data.id + ' ' + data.name + EOL);
+      queue.push('      div.panel-body ' + EOL);
       data.elements.forEach(function (el, index, array) {
-      queue.push('            article.row' + EOL);
-      queue.push('              h4.col-md-12 ' + el.keyword + ': ' + el.name + EOL);
+      queue.push('        h4.{color}.col-md-12 ' + el.name + EOL);
       el.steps.forEach(function (step) {
-      queue.push('              div.col-sm-offset-3.col-sm-9' + EOL);
-      queue.push('                h5.' + LOOKUP[step.result.status] + ' ' + step.keyword + step.name + EOL);
         progress[step.result.status]++;
-        if (step.result.status === 'undefined' && group !== 'failed') {
-          group = 'undefined';
-        } else if (step.result.status === 'failed') {
+        if (step.result.status === 'failed' || group === 'failed') {
           group = 'failed';
-        } else if (step.result.status === 'skipped' && group !== 'undefined') {
-          group = 'skipped';
+          return;
+        }
+        
+        if (step.result.status === 'skipped' || group === 'undefined') {
+          group = 'undefined';
+          return;
+        }
+        
+        if (step.result.status === 'undefined' || group === 'undefined') {
+          group = 'undefined';
+          return;
         }
       });
-      if (index !== array.length - 1) {
-      queue.push('            hr' + EOL);
-      }
       overall.push(group);
       });
       queue.push(EOL);
     }, function () {
+      if (opts.header) {
+        this.queue('header.row' + EOL);
+        this.queue('  h1.col-md-offset-2.col-md-10 ' + opts.header + EOL);
+      }
       var total = Object.keys(progress).reduce(function (prev, curr) {
         return prev + progress[curr];
       }, 0);
@@ -68,12 +67,13 @@ module.exports = function (opts) {
       if (opts.progress) {
         this.queue('+progress' + EOL);
       }
-      if (opts.header) {
-        this.queue('header.row' + EOL);
-        this.queue('  h1.col-md-offset-1.col-md-11 ' + opts.header + EOL);
-      }
+      this.queue('section.container' + EOL);
       queue.forEach(function (item) {
-        this.queue(item.replace('{color}', LOOKUP[overall.shift()]));
+        if (item.indexOf('{color}') > -1) {
+          this.queue(item.replace('{color}', LOOKUP[overall.shift()]));
+        } else {
+          this.queue(item);
+        }
       }.bind(this));
     })
   };
